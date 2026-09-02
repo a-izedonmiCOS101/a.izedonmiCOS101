85b68bf5ce9fa711f664660c32c3e9d7f6d243c0
Aviel Izedonmi <aviel.izedonmi@pau.edu.ng>
2026-09-02 22:27:21 +0100

    (My proper second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main() {
+	
+	println!(); // prints just a
+
+	println!("hello ");//prints
+
+	println!("format {} arguments", "some" ); //prints format some arguments 
+
+	println!("My name is {}! I'm not the class rep. For the twentieth time!", "Aviel");
+
+}
